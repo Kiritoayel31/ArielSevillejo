5c9e2e72c9c686800e63d4481f2fa95c52f9bd73
Ariel <arielsevillejo@gmail.com>
2023-03-11 11:41:35 +0800

    3-11-2023

---
+++ app/src/main/java/com/example/helloandroid/MainActivity3.java
@@ -0,0 +1,36 @@
+package com.example.helloandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity3";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Log.v(TAG, "This is verbose log");
+        Log.d(TAG, "This is debug log");
+        Log.i(TAG, "This is info log");
+        Log.w(TAG, "This is warn log");
+        Log.e(TAG, "This is an error log");
+
+
+        Button button = (Button) findViewById(R.id.buttonA);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/helloandroid/MainActivity4.java
@@ -0,0 +1,37 @@
+package com.example.helloandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity4 extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        Log.v(TAG, "This is verbose log");
+        Log.d(TAG, "This is debug log");
+        Log.i(TAG, "This is info log");
+        Log.w(TAG, "This is warn log");
+        Log.e(TAG, "This is an error log");
+
+
+        Button button = (Button) findViewById(R.id.sendButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
